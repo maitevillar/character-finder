80ae3d88eaa826a97ad5bbee3242f8628ee91a21
maitevillar <sayhello.maite@gmail.com>
2020-04-25 17:43:27 +0200

    styling done

--- src/components/Emptyresult.js
+++ src/components/Emptyresult.js
@@ -7,8 +7,8 @@ const Emptyresult = (props) => {
 
     return (
         <div className="empty-result-container">
+            <p className="empty-result-container__text"> No hay ningun personaje que coincida con <br/> <span className="error-text highlight">{name}</span></p>
             <img className="empty-result-container__picture" src="https://media.giphy.com/media/3o7aDbeo34WHHejJ2E/giphy.gif" alt="head-morty-gif" />
-            <p className="empty-result-container__text"> No hay ningun personaje que coincida con <span className="highlight">{name}</span></p>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Header = () => (
 	<div className="header">
+		<p className="header__text"> Character finder </p>
        
         <img src="../images/logo-rick.jpg" alt=""/>
 
