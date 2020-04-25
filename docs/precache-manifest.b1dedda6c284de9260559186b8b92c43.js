43db01ac2f557fcf265d6fb366a34769b92e8386
maitevillar <sayhello.maite@gmail.com>
2020-04-25 19:59:31 +0200

    public pages

---
+++ docs/precache-manifest.b1dedda6c284de9260559186b8b92c43.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75f64428b0291b017fdc580d2818d23",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0764c797bba1de23839",
+    "url": "./static/css/main.d1ca53a5.chunk.css"
+  },
+  {
+    "revision": "6fbaa1212237aa4c4e2f",
+    "url": "./static/js/2.3a40153a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3a40153a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0764c797bba1de23839",
+    "url": "./static/js/main.bc362edc.chunk.js"
+  },
+  {
+    "revision": "8f5a42122b02c55cd6e9",
+    "url": "./static/js/runtime-main.68fdf9d9.js"
+  },
+  {
+    "revision": "04bf845741f93dd8e73818b9d49e0f1a",
+    "url": "./static/media/blue2.04bf8457.jpg"
+  },
+  {
+    "revision": "d9337ee9f3fe5dd2206160486ac20770",
+    "url": "./static/media/blue2V.d9337ee9.jpg"
+  },
+  {
+    "revision": "97719c155b0160e81496b07c01a5cd82",
+    "url": "./static/media/dead.97719c15.png"
+  },
+  {
+    "revision": "2de7d9f30c4e41c2e8d52bd39257beb5",
+    "url": "./static/media/main-logo.2de7d9f3.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6359ced8349d795495652ed44e41dbe.js"
+  "./precache-manifest.b1dedda6c284de9260559186b8b92c43.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
